bd82f9803abdeebbaff01c79b7bef45a21e37816
Jason Liszka <jliszka@alumni.cmu.edu>
2014-03-05 18:27:07 -0500

    scala 2.10.3

--- project/Default.scala
+++ project/Default.scala
@@ -70,7 +70,7 @@ object Default {
 
   val scala: Seq[Setting[_]] = Default.all ++ Seq(
     Keys.scalaVersion := "2.9.1",
-    Keys.crossScalaVersions := Seq("2.9.1", "2.9.2", "2.10.2"),
+    Keys.crossScalaVersions := Seq("2.9.1", "2.9.2", "2.10.3"),
     Keys.scalacOptions <++= (Keys.scalaVersion).map(v => {
       val opts =
         Seq(
