585640a96a2085983709a5730a4dda994ff40f82
Jason Liszka <jliszka@alumni.cmu.edu>
2014-05-01 17:27:54 -0400

    handle null binary value in TReadableJSONProtocol

--- src/test/scala/com/foursquare/spindle/runtime/TReadableJSONProtocolTest.scala
+++ src/test/scala/com/foursquare/spindle/runtime/TReadableJSONProtocolTest.scala
@@ -4,8 +4,8 @@ package com.foursquare.spindle.runtime.test
 
 import com.foursquare.common.thrift.json.TReadableJSONProtocol
 import com.foursquare.spindle.{Record, MetaRecord}
-import com.foursquare.spindle.test.gen.TestStruct
-import org.apache.thrift.TBase
+import com.foursquare.spindle.test.gen.{TestStruct, TestStructNoUnknownFieldsTracking}
+import org.apache.thrift.{TBase, TDeserializer}
 import org.apache.thrift.transport.TMemoryInputTransport
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -14,8 +14,11 @@ class TReadableJSONProtocolTest {
 
   @Test
   def testNullValues {
-    val t1 = parseJson("""{"aString":null}""", TestStruct)
+    val t1 = deserializeJson("""{"aString":null, "unknown": null}""", TestStruct)
     assertEquals(None, t1.aStringOption)
+
+    val t2 = deserializeJson("""{"aString":null, "unknown": null}""", TestStructNoUnknownFieldsTracking)
+    assertEquals(None, t2.aStringOption)
   }
 
   def parseJson[R <: Record[R] with TBase[R, _]](s: String, recMeta: MetaRecord[R]): R = {
@@ -24,6 +27,13 @@ class TReadableJSONProtocolTest {
     val iprot = new TReadableJSONProtocol(trans, null)
     val rec = recMeta.createRawRecord
     rec.read(iprot)
+    rec
+  }
+
+  def deserializeJson[R <: Record[R] with TBase[R, _ <: org.apache.thrift.TFieldIdEnum]](s: String, recMeta: MetaRecord[R]): R = {
+    val deserializer = new TDeserializer(new TReadableJSONProtocol.Factory())
+    val rec = recMeta.createRawRecord
+    deserializer.deserialize(rec, s.getBytes("UTF-8"))
     rec
   }
 }
