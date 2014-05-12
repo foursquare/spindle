dd322049dc0e541fb79927837e83f4c66d67a78a
Jason Liszka <jliszka@alumni.cmu.edu>
2014-05-12 11:55:15 -0400

    2.0.0-M5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'spindle'
-copyright = u'2014, Jorge Ortiz, Jason Liszka'
+copyright = u'2014, Foursquare Labs, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
