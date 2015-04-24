e10fc41f6952ad73492ee4de66e494a17b6faf62
David Taylor <tinystatemachine@gmail.com>
2015-04-24 16:47:49 +0000

    Include cause trace in non-string map key errors

--- src/main/java/com/foursquare/common/thrift/base/NonStringMapKeyException.java
+++ src/main/java/com/foursquare/common/thrift/base/NonStringMapKeyException.java
@@ -5,6 +5,13 @@ import org.apache.thrift.TException;
 
 public class NonStringMapKeyException extends TException {
   public NonStringMapKeyException(Object key) {
-    super("Protocol requires string-typed map key, but got a " + key.getClass().getCanonicalName());
+    this(key, null);
+  }
+
+  public NonStringMapKeyException(Object key, Exception cause) {
+    super(
+      "Protocol requires string-typed map key, but got a " + key.getClass().getCanonicalName(),
+      cause
+    );
   }
 }
