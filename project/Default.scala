a1e03cbe9ef055c43233a62b07c815296a864156
Tom Dyas <tdyas@foursquare.com>
2015-06-04 17:07:52 -0400

    release v3.0.0-M12

--- project/Default.scala
+++ project/Default.scala
@@ -10,7 +10,7 @@ object Default {
   lazy val IvyDefaultConfiguration = config("default") extend(Compile)
   val all: Seq[Setting[_]] = Seq(
     Keys.target <<= (Keys.name)(name => Path.absolute(file("target") / name)),
-    Keys.version := "3.0.0-M11",
+    Keys.version := "3.0.0-M12",
     Keys.organization := "com.foursquare",
     Keys.ivyConfigurations += IvyDefaultConfiguration,
     Keys.publishMavenStyle := true,
